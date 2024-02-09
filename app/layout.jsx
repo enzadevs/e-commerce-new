7196bd048e673661fdf1ec278aa825a92e34cec0
Enza <e.eziz5@yandex.ru>
2024-02-09 22:56:58 +0500

    commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,23 @@
-import './globals.css'
-import TopProgressBar from 'components/Functions/TopProgressBar'
+import "./globals.css";
 
 export const metadata = {
-    title: 'Next JS',
-    description: 'Next JS',
-    name: 'viewport', 
-    content:'width=device-width, initial-scale=1',
-}
+  title: "Next JS",
+  description: "Next JS",
+  name: "viewport",
+  content: "width=device-width, initial-scale=1",
+};
 
-import {Inter} from 'next/font/google'
+import { Inter } from "next/font/google";
 
 const globalFont = Inter({
-    subsets: ['latin','cyrillic'],
-    display: 'swap'
-})
+  subsets: ["latin", "cyrillic"],
+  display: "swap",
+});
 
-export default function RootLayout({children}){
-    return(
-        <html lang='en' className={globalFont.className} suppressHydrationWarning>
-            <body className='bg-white flex flex-col min-h-screen'>
-                <TopProgressBar/>
-                {children}
-            </body>
-        </html>
-    )
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={globalFont.className} suppressHydrationWarning>
+      <body className="bg-white flex flex-col min-h-screen">{children}</body>
+    </html>
+  );
 }
