45a117999225e58ef396f27d7a7318f947e8ce39
Enza <e.eziz5@yandex.ru>
2024-03-09 16:19:40 +0500

    Update SearchBox.jsx

--- components/Nav/SearchBox.jsx
+++ components/Nav/SearchBox.jsx
@@ -1,18 +1,32 @@
 "use client";
 
+import { useRef } from "react";
 import { BiSearch } from "react-icons/bi";
 
 export default function SearchBox() {
+  const queryRef = useRef();
+
   return (
     <div className="flex-grow flex-row-center relative h-11 w-full md:w-auto">
       <input
         type="text"
+        ref={queryRef}
         className="search-input w-full"
         placeholder="Поиск..."
         minLength={2}
         maxLength={64}
+        onKeyDown={(event) => {
+          if (event.key === "Enter" && queryRef.current.value.length >= 2) {
+            window.location.href = `/search/` + queryRef.current.value;
+          }
+        }}
       ></input>
-      <button className="bg-calm rounded-r-3xl absolute center right-0 h-9 sm:h-11 w-9 sm:w-11">
+      <button
+        onClick={() => {
+          window.location.href = `/search/` + queryRef.current.value;
+        }}
+        className="bg-calm rounded-r-3xl absolute center right-0 h-9 sm:h-11 w-9 sm:w-11"
+      >
         <BiSearch className="text-white h-6 w-6" />
       </button>
     </div>
