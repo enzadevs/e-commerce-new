ce3e23a9c287233cf8317532a5925f9c85924275
Enza <e.eziz5@yandex.ru>
2024-03-26 18:57:59 +0500

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,15 +1,3 @@
-// import createMiddleware from "next-intl/middleware";
-// import { locales, localePrefix } from "./navigation";
-
-// export default createMiddleware({
-//   defaultLocale: "ru",
-//   localePrefix,
-//   locales,
-// });
-
-// export const config = {
-//   matcher: ["/", "/(ru|tm)/:path*"],
-// };
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
